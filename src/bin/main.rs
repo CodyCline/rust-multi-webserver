0366ededcf8ae3b6ab0f93640d4d5cdd171220a3
Cody Cline <codycline@system76-pc.localdomain>
2020-07-06 10:30:19 -0700

    Added implementations for multi threads, pools, workers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,18 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use rust_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    let pool = ThreadPool::new(8);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+        
     }
 }
 
@@ -31,3 +35,5 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
+
+
