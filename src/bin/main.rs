0b9cbedd73a13fea5e6d40486fbfba3bbad3b9e7
Cody Cline <codycline@system76-pc.localdomain>
2020-07-06 11:26:12 -0700

    Added graceful shutdown, threads with uuids

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,22 @@
+use rust_server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
-use rust_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(8);
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
+
         pool.execute(|| {
             handle_connection(stream);
         });
-        
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,36 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
+use uuid::Uuid;
+
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
-};
+    sender: mpsc::Sender<Message>,
+}
 
-struct Job;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let (send, receiver) = mpsc::channel();
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
 
         let mut workers = Vec::with_capacity(size);
         
-        for id in 0..size {
-            workers.push(Worker::new(id));
+        for _ in 0..size {
+            let uuid = Uuid::new_v4();
+            workers.push(Worker::new(uuid, Arc::clone(&receiver)));
         }
 
         ThreadPool { workers, sender }
@@ -26,21 +40,59 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 
 struct Worker {
-    id: usize,
-    thread: thread::JoinHandle<()>,
+    id: Uuid,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new (id:usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new (id:Uuid, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-        Worker {id, thread}
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
-
-fn main() {}
